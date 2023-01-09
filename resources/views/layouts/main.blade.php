f4c81b2faf6754241da7face2ddf26e756315663
Carmelo Palumbo <carmelopalumbo@outlook.it>
2023-01-09 14:36:16 +0100

    add-model-and-controller

--- resources/views/layouts/main.blade.php
+++ resources/views/layouts/main.blade.php
@@ -5,11 +5,12 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <link rel="icon" type="image/x-icon" href="favicon.png">
 
     <!-- Styles -->
     @vite('resources/js/app.js')
 
-    <title>Document</title>
+    <title>SUPERMOVIES</title>
 </head>
 
 <body>
